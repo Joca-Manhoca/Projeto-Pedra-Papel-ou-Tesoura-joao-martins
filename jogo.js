95739475d1dca735b5b8877aa50c158a32df6195
Joca-Manhoca <jonimaninh@gmail.com>
2025-05-16 09:57:45 +0100

    pedra papel tesoura bot

--- jogo.js
+++ jogo.js
@@ -1,1 +1,13 @@
-
+function getComputerChoice(){
+    let n = Math.random();
+    if(n==1){
+        return "rock";
+    } else if (n==2){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getComputerChoice()); 
+console.log(getComputerChoice());
+console.log(getComputerChoice());
