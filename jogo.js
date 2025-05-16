a03da1cb95b5f04265e1c95e2158777d6036ac04
Joca-Manhoca <jonimaninh@gmail.com>
2025-05-16 14:52:44 +0100

    jogo com erro que não entendo

--- jogo.js
+++ jogo.js
@@ -15,33 +15,32 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let choice = prompt("write if you want rock or paper or scissors");
-    return "you choose", choice;
+    let choose = prompt("write what you want to use between rock or paper or scissors");
+    let choice = choose.toLowerCase;
+    return choice;
 }
 
 function  playRound(humanChoice, computerChoice){
-    console.log(getComputerChoice());
-    console.log(getHumanChoice().toLowerCase());
     humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
     if (humanChoice == "scissors" && computerChoice == "rock") {
         computerScore = computerScore + 1;
-        return "you lose the score is you ${humanChoice} - computer ${computerChoice}";
+        return "you lose the score is you", humanChoice, "- computer", computerChoice;
     } else if (humanChoice == "rock" && computerChoice == "paper") {
         computerScore = computerScore + 1;
-        return "you lose the score is you ${humanChoice} - computer ${computerChoice}";
+        return "you lose the score is you", humanChoice, "- computer", computerChoice;
     } else if (humanChoice == "paper" && computerChoice == "scissors") {
         computerScore = computerScore + 1;
-        return "you lose the score is you ${humanChoice} - computer ${computerChoice}";
+        return "you lose the score is you", humanChoice, "- computer", computerChoice;
     } else if (humanChoice == "paper" && computerChoice == "rock"){
         humanScore = humanChoice + 1;
-        return "you win the score is you ${humanChoice} - computer ${computerChoice}";
+        return "you win the score is you", humanChoice, "- computer", computerChoice;
     } else if (humanChoice == "scissors" && computerChoice == "paper"){
         humanScore = humanChoice + 1;
-        return "you win the score is you ${humanChoice} - computer ${computerChoice}";
+        return "you win the score is you", humanChoice, "- computer", computerChoice;
     } else if (humanChoice == "rock" && computerChoice == "scissors"){
         humanScore = humanChoice + 1;
-        return "you win the score is you ${humanChoice} - computer ${computerChoice}";
+        return "you win the score is you", humanChoice, "- computer", computerChoice;
     }
 }
 
