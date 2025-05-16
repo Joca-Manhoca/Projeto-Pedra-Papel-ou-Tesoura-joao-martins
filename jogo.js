369497a3e88a746242e2a51a07670f8d5366887e
Joca-Manhoca <jonimaninh@gmail.com>
2025-05-16 11:54:30 +0100

    playround feirto

--- jogo.js
+++ jogo.js
@@ -1,7 +1,7 @@
-const botchoice = 0;
-const humanchoice = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-console.log("Jogo de pedra, papel, tesoura!");
+console.log("rock, paper, scissors game!");
 
 function getComputerChoice(){
     let choice = Math.random() ;
@@ -15,7 +15,34 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let choice = prompt("Escreva qual vai usar entre a pedra, o papel e a tesoura");
-    let mensage = "Escolheste ${choice}";
+    let choice = prompt("write if you want rock or paper or scissors");
+    let mensage = "you choose ${choice}";
 }
-console.log(getHumanChoice());
+
+function  playRound(humanChoice, computerChoice){
+    humanChoice.toLowerCase() = getHumanChoice();
+    computerChoice = getComputerChoice();
+    if (humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore = computerScore + 1;
+        let  mensage = "you lose the score is you ${humanChoice} - computer ${computerChoice}";
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        computerScore = computerScore + 1;
+        let  mensage = "you lose the score is you ${humanChoice} - computer ${computerChoice}";
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        computerScore = computerScore + 1;
+        let  mensage = "you lose the score is you ${humanChoice} - computer ${computerChoice}";
+    } else if (humanChoice == "paper" && computerChoice == "rock"){
+        humanScore = humanChoice + 1;
+        let  mensage = "you win the score is you ${humanChoice} - computer ${computerChoice}";
+    } else if (humanChoice == "scissors" && computerChoice == "paper"){
+        humanScore = humanChoice + 1;
+        let  mensage = "you win the score is you ${humanChoice} - computer ${computerChoice}";
+    } else if (humanChoice == "rock" && computerChoice == "scissors"){
+        humanScore = humanChoice + 1;
+        let  mensage = "you win the score is you ${humanChoice} - computer ${computerChoice}";
+    }
+}
+
+function playGame(){
+    
+}
