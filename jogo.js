708c81d2d9654eb88fa8b4fe2d013ba6b74dbc3e
Joca-Manhoca <jonimaninh@gmail.com>
2025-05-16 10:56:50 +0100

    human e computer choices feitas

--- jogo.js
+++ jogo.js
@@ -1,13 +1,21 @@
+const botchoice = 0;
+const humanchoice = 0;
+
+console.log("Jogo de pedra, papel, tesoura!");
+
 function getComputerChoice(){
-    let n = Math.random();
-    if(n==1){
+    let choice = Math.random() ;
+    if(choice <= 0.33){
         return "rock";
-    } else if (n==2){
+    } else if (choice <= 0.66){
         return "paper";
     } else {
         return "scissors";
     }
 }
-console.log(getComputerChoice()); 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let choice = prompt("Escreva qual vai usar entre a pedra, o papel e a tesoura");
+    let mensage = "Escolheste ${choice}";
+}
+console.log(getHumanChoice());
